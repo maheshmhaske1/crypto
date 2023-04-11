ffefab504f994c1d4de87984df51aab1d972e623
Mahesh <maheshmhaske241198@gmail.com>
2023-04-11 12:29:48 +0530

    OK

--- routes/cryptoRouter.js
+++ routes/cryptoRouter.js
@@ -5,9 +5,9 @@ const authenticateUser = auth.authenticate_user
 
 const crypto = require('../controller/cryptoController')
 
-router.get('/get-news', authenticateUser, crypto.getCryptoNews)
-router.get('/get-detail/:currency_name', authenticateUser, crypto.getCryptoDetails)
-router.get('/get-topGainerLoser', authenticateUser, crypto.getTopGainerAndLoser)
-router.get('/get-top100', authenticateUser, crypto.getTop50)
+router.get('/get-news', crypto.getCryptoNews)
+router.get('/get-detail/:currency_name', crypto.getCryptoDetails)
+router.get('/get-topGainerLoser', crypto.getTopGainerAndLoser)
+router.get('/get-top100', crypto.getTop50)
 
 module.exports = router
